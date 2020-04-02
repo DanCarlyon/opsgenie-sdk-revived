cc2ebcb7f5e9d9735873d7e0b6d9e9af3dfc629a
Dan Carlyon <dan.carlyon@space48.com>
2020-04-02 21:29:29 +0100

    #1 extending apis

--- samples/configure.js
+++ samples/configure.js
@@ -3,6 +3,6 @@
 var opsgenie = require('../');
 
 opsgenie.configure({
-    'host': 'https://api.opsgenie.com',
-    'api_key': "apikey"
+    'host': 'https://api.eu.opsgenie.com',
+    'api_key': "67816958-3223-4d02-b87e-904588217b75"
 });
