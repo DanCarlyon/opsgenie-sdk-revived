f88c0b973df582e6be76bce8d51cfb67a7560306
Dan Carlyon <dan.carlyon@space48.com>
2022-06-08 13:32:02 +0100

    Remove and rotate api key

--- samples/configure.js
+++ samples/configure.js
@@ -4,5 +4,5 @@ var opsgenie = require('../');
 
 opsgenie.configure({
     'host': 'https://api.eu.opsgenie.com',
-    'api_key': "742a5746-b3fb-40a3-ac19-e1fc6a07436b"
+    'api_key': "api_key"
 });
