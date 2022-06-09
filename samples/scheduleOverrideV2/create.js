efe3fb065365af05a7ba88b1b7bfdd7a1f1c0d2f
Dan Carlyon <dan.carlyon@space48.com>
2022-06-09 10:48:11 +0100

    #25 add update and delete to schedule override

--- samples/scheduleOverrideV2/create.js
+++ samples/scheduleOverrideV2/create.js
@@ -4,21 +4,21 @@ let opsgenie = require('../../');
 require('../configure');
 
 let create_schedule_override_json = {
-    "alias" : "Override Alias",
+    "alias" : "OverrideAlias",
     "user" : {
         "type" : "user",
-        "username": "user@email.com"
+        "username": "dan.carlyon@space48.com"
     },
-    "startDate" : "2032-06-14T09:00Z",
-    "endDate" : "2032-06-14T15:00Z",
+    "startDate" : "2022-06-14T09:00Z",
+    "endDate" : "2022-06-14T15:00Z",
     "rotations" : [
         {
-            "id": "04a7e991-8db1-4404-9811-000000000000"
+            "id": "04a7e991-8db1-4404-9811-53b45465df9c"
         }
     ]
 };
 
-opsgenie.scheduleOverrideV2.create('ee263bad-e4e2-4119-a7d0-000000000000', create_schedule_override_json, function (error, overrideData) {
+opsgenie.scheduleOverrideV2.create('ee263bad-e4e2-4119-a7d0-0322b84a7680', create_schedule_override_json, function (error, overrideData) {
     if (error) {
         console.error(error);
     } else {
---
+++ samples/scheduleOverrideV2/update.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let opsgenie = require('../../');
+require('../configure');
+
+let create_schedule_override_update_json = {
+    "alias" : "OverrideAlias",
+    "user" : {
+        "type" : "user",
+        "username": "user@email.com"
+    },
+    "startDate" : "2032-06-17T09:00Z",
+    "endDate" : "2032-06-17T15:00Z",
+    "rotations" : [
+        {
+            "name": "Rota Name"
+        }
+    ]
+};
+
+opsgenie.scheduleOverrideV2.update('Override Alias', 'eb387227-4d7c-4ceb-b970-000000000000', create_schedule_override_update_json, {}, function (error, overrideData) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log("Update Schedule Override Response");
+        console.log(overrideData);
+    }
+});
